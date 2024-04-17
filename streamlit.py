ffb9bcccb9feba715b1b7819b4f23139e509af40
Navneet Shrivastava <99131078+navneetx@users.noreply.github.com>
2024-04-17 16:28:24 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1341UcL1DTj5ciQnucQLW0TGbIM6t81qR
 """
 
-!pip install streamlit
+pip install streamlit
 
 import streamlit as st
 
